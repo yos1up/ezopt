4947eb6ed852ff3cc9c97c6e3c3facbee6102d81
yos1up <yos1up@gmail.com>
2024-03-18 00:31:35 +0900

    add slice plot

--- ezopt/study_visualizer.py
+++ ezopt/study_visualizer.py
@@ -33,14 +33,21 @@ class StudyVisualizer:
                     for t in study.trials
                 ]
             }, f, indent=2, ensure_ascii=False)
+        
         optuna.visualization.plot_optimization_history(study).write_html(output_dir / "optimization_history.html")
         optuna.visualization.plot_parallel_coordinate(study).write_html(output_dir / "parallel_coordinate.html")
+        
         fig = optuna.visualization.plot_param_importances(study)
         fig.write_html(output_dir / "param_importances.html")
         params_sorted_by_importance = list(fig.data[0].y[::-1])
         # NOTE: get_param_importances で再計算するのは重たい（また seed を合わせないと結果が再現しない）ため，
         # visualization fig から重要パラメータを抽出するようにしている
-        optuna.visualization.plot_contour(study, params=params_sorted_by_importance[:3]).write_html(output_dir / "contour.html")
-        # NOTE: contour を全変数について出力すると html ファイルサイズが膨大となるので，重要度が高いと思われる 3 パラメータに絞って描画している
+        
+        if len(params_sorted_by_importance) > 1:
+            optuna.visualization.plot_contour(study, params=params_sorted_by_importance[:3]).write_html(output_dir / "contour.html")
+            # NOTE: contour を全変数について出力すると html ファイルサイズが膨大となるので，重要度が高いと思われる 3 パラメータに絞って描画している
+        
+        optuna.visualization.plot_slice(study, params=params_sorted_by_importance[:3]).write_html(output_dir / "slice.html")
+
         with open(output_dir / "study.pkl", "wb") as f:
             pickle.dump(study, f)
